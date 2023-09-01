5d6349f496e784b3e2a092c93fd4ec580361ed04
abrilxcx <abrlbjrn@gmail.com>
2023-08-31 21:37:09 -0600

    resolucion tarea

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'gifs-card-list',
   templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.css']
 })
 export class CardListComponent {
 
